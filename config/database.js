30264a964f8b3c6aa4c75d733e24092b9991913f
jslee <datadam@kakao.com>
2022-03-18 18:33:13 +0900

    220318

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', '10.10.10.10'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'cwcc_strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'crea0425'),
+        password: env('DATABASE_PASSWORD', 'Crea0425!@12'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
